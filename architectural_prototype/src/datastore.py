5e72579d42b20cfe4b2fee8524dcaebb09a38e95
Shantanu Bala <shantanubala@gmail.com>
2012-10-28 14:21:44 +0100

    added peewee ORM and SQLite backend

--- architectural_prototype/src/datastore.py
+++ architectural_prototype/src/datastore.py
@@ -1,45 +1,15 @@
-from db import SimpleDb
-    
-class Datastore(SimpleDB):
-    def __init__(self, domain="datastore"):
-        super(domain)
-    
-    def new_entry(self, truck_id, timestamp, lat, lon):
-        entry = self.domain.new_item()
-        entry['truck_id'] = truck_id
-        entry['timestamp'] = timestamp
-        entry['lat'] = lat
-        entry['lon'] = lon
-        entry.save()
-        return entry
-    
-    def new_entries(self, points):
-        values = {}
-        for p in points:
-            key = str(p['truck_id']) + str(p['timestamp'])
-            values[key] = p
-        self.domain.batch_put_attributes(values)
-    
-    def get_all_data(self):
-        return [p for p in self.domain]
-    
-    def query_by_truck(self, truck_id, min_time=None, max_time=None):
-        query_string = "'%s'='%s'" % ("truck_id", truck_id)
-        if min_time:
-            query_string += " and '%s'>'%s'" % ("timestamp", str(min_time))
-        if max_time:
-            query_string += " and '%s'<'%s'" % ("timestamp", str(max_time))
+import peewee
+
+database = peewee.SqliteDatabase('sqlite:///:memory:')
         
-        return self.domain.query("[%s]" % query_string)
+class TruckData(BaseModel):
+    class Meta:
+        database = database
     
-    def query_by_location(self, lat, lon, radius, min_time=None, max_time=None):
-        query_string = "'%s'>'%s'" % ("lat", str(lat - radius))
-        query_string += "'%s'<'%s'" % ("lat", str(lat + radius))
-        query_string += "'%s'>'%s'" % ("lon", str(lon - radius))
-        query_string += "'%s'<'%s'" % ("lon", str(lon + radius))
-        if min_time:
-            query_string += " and '%s'>'%s'" % ("timestamp", str(min_time))
-        if max_time:
-            query_string += " and '%s'<'%s'" % ("timestamp", str(max_time))
-        
-        return self.domain.query("[%s]" % query_string)
+    truck_id = peewee.IntegerField()
+    timestamp = peewee.DateTimeField()
+    latitude = peewee.FloatField()
+    longitude = peewee.FloatField()
+
+database.connect()
+TruckData.create_table()
---
+++ architectural_prototype/src/runserver.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    from wsgi import app
+    app.run()
