2d94c1c37ad695593c23caf7c37090603a465176
Shantanu Bala <shantanubala@gmail.com>
2012-10-28 13:29:25 +0100

    addd query functions

--- architectural_prototype/src/datastore.py
+++ architectural_prototype/src/datastore.py
@@ -19,3 +19,27 @@ class Datastore(SimpleDB):
             key = str(p['truck_id']) + str(p['timestamp'])
             values[key] = p
         self.domain.batch_put_attributes(values)
+    
+    def get_all_data(self):
+        return [p for p in self.domain]
+    
+    def query_by_truck(self, truck_id, min_time=None, max_time=None):
+        query_string = "'%s'='%s'" % ("truck_id", truck_id)
+        if min_time:
+            query_string += " and '%s'>'%s'" % ("timestamp", str(min_time))
+        if max_time:
+            query_string += " and '%s'<'%s'" % ("timestamp", str(max_time))
+        
+        return self.domain.query("[%s]" % query_string)
+    
+    def query_by_location(self, lat, lon, radius, min_time=None, max_time=None):
+        query_string = "'%s'>'%s'" % ("lat", str(lat - radius))
+        query_string += "'%s'<'%s'" % ("lat", str(lat + radius))
+        query_string += "'%s'>'%s'" % ("lon", str(lon - radius))
+        query_string += "'%s'<'%s'" % ("lon", str(lon + radius))
+        if min_time:
+            query_string += " and '%s'>'%s'" % ("timestamp", str(min_time))
+        if max_time:
+            query_string += " and '%s'<'%s'" % ("timestamp", str(max_time))
+        
+        return self.domain.query("[%s]" % query_string)
